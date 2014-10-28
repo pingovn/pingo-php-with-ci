6e40f7e595fd8fc3fd7bf5e93667ca1abace43ed
Tuan Duong <none@Tuans-MacBook-Pro.local>
2014-10-28 18:55:36 +0700

    Add more actions

--- src/application/controllers/welcome.php
+++ src/application/controllers/welcome.php
@@ -19,7 +19,12 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('you_view');
+	}
+
+	public function phpinfo()
+	{
+		phpinfo();
 	}
 }
 
---
+++ src/application/views/test_view.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>Tip Of The Days</title>
+	</head>
+	<body>
+		<h3>Tips of the day</h3>
+	</body>
+</html>
