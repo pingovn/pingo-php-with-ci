024b4d5cbf2bba36217f0ad54938f3a3b6b001e7
Tuan Duong <none@Tuans-MacBook-Pro.local>
2014-10-28 20:27:03 +0700

    Change path of css/js file

--- src/application/views/homepage.php
+++ src/application/views/homepage.php
@@ -6,23 +6,21 @@
 -->
 <html>
 	<head>
-		<title>ZeroFour by HTML5 UP</title>
+		<title>Tips Of The Day</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-desktop.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="/css/ie/html5shiv.js"></script><![endif]-->
+		<script src="/js/jquery.min.js"></script>
+		<script src="/js/jquery.dropotron.min.js"></script>
+		<script src="/js/skel.min.js"></script>
+		<script src="/js/skel-layers.min.js"></script>
+		<script src="/js/init.js"></script>
+		<link rel="stylesheet" type="text/css" href="/css/skel.css" />
+		<link rel="stylesheet" type="text/css" href="/css/style.css" />
+		<link rel="stylesheet" type="text/css" href="/css/style-desktop.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="/css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="/css/ie/v9.css" /><![endif]-->
 	</head>
 	<body class="homepage">
 
@@ -101,7 +99,7 @@
 								<div class="row">
 									<div class="4u">
 										<section>
-											<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+											<a href="#" class="image featured"><img src="/images/pic01.jpg" alt="" /></a>
 											<header class="second icon fa-user">
 												<h3>Here's a Heading</h3>
 												<p>And a subtitle</p>
@@ -110,7 +108,7 @@
 									</div>
 									<div class="4u">
 										<section>
-											<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+											<a href="#" class="image featured"><img src="/images/pic02.jpg" alt="" /></a>
 											<header class="second icon fa-cog">
 												<h3>Also a Heading</h3>
 												<p>And another subtitle</p>
@@ -119,7 +117,7 @@
 									</div>
 									<div class="4u">
 										<section>
-											<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+											<a href="#" class="image featured"><img src="/images/pic03.jpg" alt="" /></a>
 											<header class="second icon fa-bar-chart-o">
 												<h3>Another Heading</h3>
 												<p>And yes, a subtitle</p>
@@ -192,7 +190,7 @@
 											
 											<!-- Excerpt -->
 												<article class="box excerpt">
-													<a href="#" class="image left"><img src="images/pic04.jpg" alt="" /></a>
+													<a href="#" class="image left"><img src="/images/pic04.jpg" alt="" /></a>
 													<div>
 														<header>
 															<span class="date">July 24</span>
@@ -206,7 +204,7 @@
 
 											<!-- Excerpt -->
 												<article class="box excerpt">
-													<a href="#" class="image left"><img src="images/pic05.jpg" alt="" /></a>
+													<a href="#" class="image left"><img src="/images/pic05.jpg" alt="" /></a>
 													<div>
 														<header>
 															<span class="date">July 18</span>
@@ -220,7 +218,7 @@
 
 											<!-- Excerpt -->
 												<article class="box excerpt">
-													<a href="#" class="image left"><img src="images/pic06.jpg" alt="" /></a>
+													<a href="#" class="image left"><img src="/images/pic06.jpg" alt="" /></a>
 													<div>
 														<header>
 															<span class="date">July 14</span>
@@ -240,7 +238,7 @@
 										<section class="box spotlight">
 											<h2 class="icon fa-file-text-o">Spotlight</h2>
 											<article>
-												<a href="#" class="image featured"><img src="images/pic07.jpg" alt=""></a>
+												<a href="#" class="image featured"><img src="/images/pic07.jpg" alt=""></a>
 												<header>
 													<h3><a href="#">Neural Implants</a></h3>
 													<p>The pros and cons. Mostly cons.</p>
