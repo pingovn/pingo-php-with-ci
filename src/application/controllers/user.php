94badcac4e7e9377defa4e34dc543267f9beecab
minhtrieu <ttminh_111@yahoo.com>
2014-11-06 20:48:10 +0700

    sync with master

---
+++ src/application/controllers/user.php
@@ -0,0 +1,65 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->helper('url');
+        $this->load->model("PingoModel");
+    }
+
+    public function register()
+    {
+        $this->load->model("Users", "userModel"); //minhtrieu: alias Users to userModel. Then, userModel is used instead of Users.
+                                                    //purpose: for clear coding style:  after -> is lowcase char
+        $errorMessage = '';
+        if (isset($_POST['btnRegister'])) {
+            // Processing registering new account
+            $email = $this->input->post('txtEmail');
+            $this->load->helper('email');
+            if (!valid_email($email)) {
+                $errorMessage =  'email is invalid';
+                $this->load->view("layout/layout", array(
+                    'errorMessage'  => $errorMessage,
+                    'mainContent'   => VIEW_PATH . '/user/register.php'
+                    ));
+                return;
+            }
+
+            $password = $this->input->post('txtPassword');
+            $confirmPassword = $this->input->post('txtConfirmPassword');
+
+            // Kiem tra email form name@domain.com
+            // So sanh password voi confirm password
+            // Kiem tra email co ton tai 
+            // Password > 6 ky tu
+            $user = array(
+                'email'         => $email,
+                'password'  => $password
+            );
+
+            $userId = $this->userModel->createUser($user);
+
+            if ($userId === false) {
+                $errorMessage = "Can not create new user. Please try again";
+            } else {
+                return $this->login();    
+            }
+        }
+        $this->load->view("layout/layout", array(
+            'errorMessage'  => $errorMessage,
+            'mainContent'   => VIEW_PATH . '/user/register.php'
+            ));
+    }
+
+    function login()
+    {
+        $this->load->view("layout/layout", array(
+            'mainContent'   => VIEW_PATH . '/user/login.php'
+            ));
+    }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
