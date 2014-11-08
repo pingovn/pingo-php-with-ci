3c3164e51dc086fe3fb9bc9f322610bc8af404e4
Tuan Duong <bacduong@gmail.com>
2014-11-08 19:15:49 +0700

    Fix copy-paste

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://ci.dev;
+$config['base_url']	= 'http://ci.dev';
 
 /*
 |--------------------------------------------------------------------------
