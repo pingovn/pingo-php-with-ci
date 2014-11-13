b474fb1c5c4ee14acf0f4d9d4e9133a45090d66d
Storm <phucnguyen19@gmail.com>
2014-11-13 19:08:51 +0700

    show image at v_upload

--- src/application/views/user/v_upload.php
+++ src/application/views/user/v_upload.php
@@ -3,9 +3,8 @@
 <section class="container">
     <div class="login">
         <h1>Change avatar</h1>
-
         <?php echo form_open_multipart('c_user/do_upload');?>
-        <img src="/themes/phatnguyen/theme3/images/" width=80px height=80px border=0px />
+        <img src="/themes/phatnguyen/theme3/uploads/<?php echo $userinfo['avatar']?>" width=80px height=80px border=0px />
         <input type="file" name="userfile" size="20" />
         <input type="submit" value="upload" name="upload" />
         </form>
